565a700fb88701319a783a2a9215b21c57c08d96
Matej Matiasko <matej.matiasko@gmail.com>
2017-02-27 17:48:37 +0100

    Fix tests

--- src/__tests__/server.spec.ts
+++ src/__tests__/server.spec.ts
@@ -1,7 +1,6 @@
 import * as request from 'supertest';
 import server from '../server';
 import * as path from 'path';
-import { escape } from 'querystring';
 import listAllFixtures from '../listAllFixtures';
 import {emptyDirSync} from 'fs-extra';
 import {spawn, ChildProcess} from 'child_process';
@@ -149,7 +148,7 @@ describe('Stub server', () => {
       .expect(200)
       .then((res: request.Response) => {
         expect(res.body.variants).toEqual(['/foo/bar/GET.unauthorized', '/fooBar/GET.blacklisted']);
-        expect(res.header['set-cookie'][0]).toEqual(`variants=${escape('/foo/bar/GET.unauthorized,/fooBar/GET.blacklisted')}; Path=/`);
+        expect(res.header['set-cookie'][0]).toEqual(`variants=/foo/bar/GET.unauthorized,/fooBar/GET.blacklisted; Path=/`);
       });
   });
 
@@ -171,7 +170,7 @@ describe('Stub server', () => {
       .expect(200)
       .then((res: request.Response) => {
         expect(res.body.variants).toEqual(['/fooBar/GET.blacklisted', '/foo/GET.blacklisted']);
-        expect(res.header['set-cookie'][0]).toEqual(`variants=${escape('/fooBar/GET.blacklisted,/foo/GET.blacklisted')}; Path=/`);
+        expect(res.header['set-cookie'][0]).toEqual(`variants=/fooBar/GET.blacklisted,/foo/GET.blacklisted; Path=/`);
       });
   });
 
@@ -182,7 +181,7 @@ describe('Stub server', () => {
       .expect(200)
       .then((res: request.Response) => {
         expect(res.body.variants).toEqual(['/foo/bar/GET.unauthorized', '/fooBar/GET.unauthorized']);
-        expect(res.header['set-cookie'][0]).toEqual(`variants=${escape('/foo/bar/GET.unauthorized,/fooBar/GET.unauthorized')}; Path=/`);
+        expect(res.header['set-cookie'][0]).toEqual(`variants=/foo/bar/GET.unauthorized,/fooBar/GET.unauthorized; Path=/`);
 
     });
   });
